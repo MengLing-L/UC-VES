a751ca459bb48df002fdf99cc730441b5b68a737
mengling@connect.hku.hk <mengling@connect.hku.hk>
2022-06-07 14:13:58 +0800

    encrypt

--- depends/COCO-framework/encrypt_witness.hpp
+++ depends/COCO-framework/encrypt_witness.hpp
@@ -12,163 +12,158 @@ this hpp implements NIZKPoK for discrete logarithm equality
 #include "../common/hash.hpp"
 #include "../common/print.hpp"
 #include "../common/routines.hpp"
+#include "../COCO-framework/nizk_dlog.hpp"
+#include "../twisted_elgamal/twisted_elgamal.hpp"
+
+const size_t DLOG_SIZE = 3;
 
 struct Witness_Encryption_AndR_PP
 {
-    EC_POINT *g, *h; 
-    size_t VECTOR_LEN; // VECTOR_LEN = 4;
-    EC_POINT *EK_R;
-    EC_POINT *EK_COCO;
+    DLOG_PP dlog_pp_unsig;
+    DLOG_PP dlog_pp_sig;
 };
 
 struct Witness_Encryption_AndR_Instance
 { 
-    
+    vector<DLOG_Instance> dlog_instance;
 }; 
 
 struct Witness_Encryption_AndR_Witness
 {
-    vector<Range_Witness> range_witness;
+    vector<DLOG_Witness> dlog_witness;
 }; 
  
 struct Witness_Encryption_AndR_Proof
-{
+{   
+    vector<DLOG_Proof> dlog_proof;
     
 };
 
 void NIZK_Witness_Encryption_AndR_PP_new(Witness_Encryption_AndR_PP &pp){
-    pp.g = EC_POINT_new(group);
-    pp.h = EC_POINT_new(group);
+    NIZK_DLOG_PP_new(pp.dlog_pp_unsig);
+    NIZK_DLOG_PP_new(pp.dlog_pp_sig);
 }
 
 void NIZK_Witness_Encryption_AndR_PP_free(Witness_Encryption_AndR_PP &pp)
 { 
-    EC_POINT_free(pp.g); 
-    EC_POINT_free(pp.h); 
+    NIZK_DLOG_PP_free(pp.dlog_pp_unsig);
+    NIZK_DLOG_PP_free(pp.dlog_pp_sig);
 }
 
 void NIZK_Witness_Encryption_AndR_Instance_new(Witness_Encryption_AndR_Instance &instance)
-{
-    
-    instance.C = EC_POINT_new(group);
+{   
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Instance_new(instance.dlog_instance[i]);
+    }
 }
 
 void NIZK_Witness_Encryption_AndR_Instance_free(Witness_Encryption_AndR_Instance &instance)
 {
-    EC_POINT_free(instance.C);
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Instance_free(instance.dlog_instance[i]);
+    }
 }
 
 void NIZK_Witness_Encryption_AndR_Witness_new(Witness_Encryption_AndR_Witness &witness)
 {
-    witness.w = BN_new();
-    witness.r = BN_new();
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Witness_new(witness.dlog_witness[i]);
+    }
 }
 
 void NIZK_Witness_Encryption_AndR_Witness_free(Witness_Encryption_AndR_Witness &witness)
 {
-    BN_free(witness.w);
-    BN_free(witness.r);
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Witness_free(witness.dlog_witness[i]);
+    }
 }
 
-void NIZK_Witness_Encryption_AndR_Proof_new(Witness_Encryption_AndR_Proof &proof, Witness_Encryption_AndR_PP &pp)
+void NIZK_Witness_Encryption_AndR_Proof_new(Witness_Encryption_AndR_Proof &proof)
 {
-    proof.delta = "";
-    proof.chl = BN_new();
-    proof.tau = BN_new();
-    proof.c.resize(pp.VECTOR_LEN); 
-    proof.z.resize(pp.VECTOR_LEN); 
-    proof.t.resize(pp.VECTOR_LEN); 
-    ECP_vec_new(proof.c);
-    BN_vec_new(proof.z); 
-    BN_vec_new(proof.t);
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Proof_new(proof.dlog_proof[i]);
+    }
 }
 
 void NIZK_Witness_Encryption_AndR_Proof_free(Witness_Encryption_AndR_Proof &proof)
 {
   
-    ECP_vec_free(proof.c);
-    BN_free(proof.chl);
-    BN_free(proof.tau);
-    BN_vec_free(proof.z); 
-    BN_vec_free(proof.t);
-    proof.c.resize(0);
+    for (int i=0; i < DLOG_SIZE; i++){
+        NIZK_DLOG_Proof_free(proof.dlog_proof[i]);
+    }
 }
 
 void Witness_Encryption_AndR_PP_print(Witness_Encryption_AndR_PP &pp)
 {
-    cout << "Witness_Encryption_AndR Proofs Public parameters >>> " << endl;
-    ECP_print(pp.g, "pp.g"); 
-    ECP_print(pp.h, "pp.h"); 
-    cout << "VECTOR_LEN: " << pp.VECTOR_LEN << endl;
+    DLOG_PP_print(pp.dlog_pp_unsig);
+    DLOG_PP_print(pp.dlog_pp_sig);
 }
 
 void Witness_Encryption_AndR_Instance_print(Witness_Encryption_AndR_Instance &instance)
 {
-    cout << "Witness_Encryption_AndR Proofs Instance >>> " << endl;  
-    ECP_print(instance.C, "instance.c"); 
+    for (int i=0; i < DLOG_SIZE; i++){
+        DLOG_Instance_print(instance.dlog_instance[i]);
+    } 
     
 } 
 
 void Witness_Encryption_AndR_Witness_print(Witness_Encryption_AndR_Witness &witness)
 {
-    cout << "Witness_Encryption_AndR Proofs Witness >>> " << endl; 
-    BN_print(witness.w, "w"); 
-    BN_print(witness.r, "r"); 
+    for (int i=0; i < DLOG_SIZE; i++){
+        DLOG_Witness_print(witness.dlog_witness[i]);
+    } 
 } 
 
 void Witness_Encryption_AndR_Proof_print(Witness_Encryption_AndR_Proof &proof)
 {
-    SplitLine_print('-'); 
-    cout << "NIZKPoK for Witness_Encryption_AndR Proofs >>> " << endl; 
-    cout << "proof.delta: " << proof.delta << endl;
-    ECP_vec_print(proof.c, "proof.c");
-    BN_print(proof.chl, "proof.chl"); 
-    BN_print(proof.tau, "proof.tau");
-    BN_vec_print(proof.z, "proof.z");
-    BN_vec_print(proof.t, "proof.t");
+    for (int i=0; i < DLOG_SIZE; i++){
+        DLOG_Proof_print(proof.dlog_proof[i]);
+    } 
 }
 
-void Witness_Encryption_AndR_Proof_serialize(Witness_Encryption_AndR_Proof &proof, ofstream &fout)
-{ 
-    ECP_vec_serialize(proof.c, fout);
-
-    BN_serialize(proof.tau,  fout);
-    BN_serialize(proof.chl,  fout);
-} 
-
-void Witness_Encryption_AndR_Proof_deserialize(Witness_Encryption_AndR_Proof &proof, ifstream &fin)
-{
-    
-    ECP_vec_deserialize(proof.c, fin);
-
-    BN_deserialize(proof.tau,  fin);
-    BN_deserialize(proof.chl,  fin);
-} 
 
 
-void NIZK_Witness_Encryption_AndR_Setup(Witness_Encryption_AndR_PP &pp, size_t VECTOR_LEN){
-    EC_POINT_copy(pp.g, generator); 
-    //EC_POINT_copy(pp.h, h);
-    Hash_ECP_to_ECP(pp.g, pp.h);
-    pp.VECTOR_LEN = VECTOR_LEN;
+void NIZK_Witness_Encryption_AndR_Setup(Witness_Encryption_AndR_PP &pp, EC_POINT* &h, EC_POINT* &EK){
+    NIZK_DLOG_Setup(pp.dlog_pp_sig, h, EK, true);
+    NIZK_DLOG_Setup(pp.dlog_pp_unsig, h, EK, false);
 }
 
-void NIZK_Witness_Encryption_AndR_Prove(Witness_Encryption_AndR_PP &pp, 
+void NIZK_Witness_Encryption_AndR_Prove_Compute_Chl(Witness_Encryption_AndR_PP &pp, 
                             Witness_Encryption_AndR_Instance &instance, 
-                            Witness_Encryption_AndR_Witness &witness, 
+                            Witness_Encryption_AndR_Witness &witness,
+                            string &chl, 
                             Witness_Encryption_AndR_Proof &proof){
-    //hard code C
-
     
+    NIZK_DLOG_Prove_Compute_Chl(pp.dlog_pp_unsig, instance.dlog_instance[0], witness.dlog_witness[0], chl, proof.dlog_proof[0]);
+    NIZK_DLOG_Prove_Compute_Chl(pp.dlog_pp_unsig, instance.dlog_instance[1], witness.dlog_witness[1], chl, proof.dlog_proof[1]);
+    NIZK_DLOG_Prove_Compute_Chl(pp.dlog_pp_sig, instance.dlog_instance[2], witness.dlog_witness[2], chl, proof.dlog_proof[2]);
+
+}
+
+void NIZK_Witness_Encryption_AndR_Prove_Compute_Proof(Witness_Encryption_AndR_PP &pp, 
+                            Witness_Encryption_AndR_Instance &instance, 
+                            Witness_Encryption_AndR_Witness &witness,
+                            string &chl, 
+                            Witness_Encryption_AndR_Proof &proof){
+    
+    NIZK_DLOG_Prove_Compute_Proof(pp.dlog_pp_unsig, instance.dlog_instance[0], witness.dlog_witness[0], chl, proof.dlog_proof[0]);
+    NIZK_DLOG_Prove_Compute_Proof(pp.dlog_pp_unsig, instance.dlog_instance[1], witness.dlog_witness[1], chl, proof.dlog_proof[1]);
+    NIZK_DLOG_Prove_Compute_Proof(pp.dlog_pp_sig, instance.dlog_instance[2], witness.dlog_witness[2], chl, proof.dlog_proof[2]);
+
 }
 
 
 bool NIZK_Witness_Encryption_AndR_Verify(Witness_Encryption_AndR_PP &pp, 
                             Witness_Encryption_AndR_Instance &instance, 
                             Witness_Encryption_AndR_Witness &witness, 
-                            Witness_Encryption_AndR_Proof &proof){
+                            string &chl, 
+                            Witness_Encryption_AndR_Proof &proof,
+                            string &res){
 
     
+    NIZK_DLOG_Verify(pp.dlog_pp_unsig, instance.dlog_instance[0], chl, proof.dlog_proof[0], res);
+    NIZK_DLOG_Verify(pp.dlog_pp_unsig, instance.dlog_instance[1], chl, proof.dlog_proof[1], res);
+    NIZK_DLOG_Verify(pp.dlog_pp_sig, instance.dlog_instance[2], chl, proof.dlog_proof[2], res);
 
 }
 
---
+++ test/test_encrypt_witness.cpp
@@ -0,0 +1,107 @@
+#define DEBUG
+
+#include "../depends/COCO-framework/encrypt_witness.hpp"
+#include "../depends/twisted_elgamal/twisted_elgamal.hpp"
+#include <string.h>
+#include <vector> 
+using namespace std;
+
+
+
+void test_protocol()
+{
+    SplitLine_print('-'); 
+    cout << "Initialization >>>" << endl;
+    
+
+    Twisted_ElGamal_PP enc_pp; 
+    Twisted_ElGamal_PP_new(enc_pp);
+    size_t MSG_LEN = 32; 
+    size_t TUNNING = 7; 
+    size_t DEC_THREAD_NUM = 4;
+    size_t IO_THREAD_NUM = 4;      
+    Twisted_ElGamal_Setup(enc_pp, MSG_LEN, TUNNING, DEC_THREAD_NUM, IO_THREAD_NUM);
+    Twisted_ElGamal_Initialize(enc_pp); 
+
+    Twisted_ElGamal_KP keypair;
+    Twisted_ElGamal_KP_new(keypair); 
+    Twisted_ElGamal_KeyGen(enc_pp, keypair); 
+
+    Twisted_ElGamal_CT CT; 
+    Twisted_ElGamal_CT_new(CT); 
+
+    Witness_Encryption_AndR_PP pp;
+    NIZK_Witness_Encryption_AndR_PP_new(pp);
+    NIZK_Witness_Encryption_AndR_Setup(pp, enc_pp.h, keypair.pk);
+    Witness_Encryption_AndR_Instance instance;
+    NIZK_Witness_Encryption_AndR_Instance_new(instance);
+    Witness_Encryption_AndR_Witness witness;
+    NIZK_Witness_Encryption_AndR_Witness_new(witness);
+    
+    
+    Witness_Encryption_AndR_Proof proof;
+    NIZK_Witness_Encryption_AndR_Proof_new(proof);
+
+    for (int i=0; i < 3; i++){
+        BN_hex2bn(&witness.dlog_witness[i].w, "65BB42E0");
+        BN_hex2bn(&witness.dlog_witness[i].gamma, "65BB42E6");
+        BN_mod(witness.dlog_witness[i].w, witness.dlog_witness[i].w, enc_pp.BN_MSG_SIZE, bn_ctx);
+        BN_print(witness.dlog_witness[i].w, "dlog_witness.w"); 
+        Twisted_ElGamal_Enc(enc_pp, keypair.pk, witness.dlog_witness[i].w, witness.dlog_witness[i].gamma, CT);
+        //Twisted_ElGamal_Dec(pp, keypair.sk, CT, m_prime); 
+        //BN_print(m_prime, "m'"); 
+
+        EC_POINT_copy(instance.dlog_instance[i].U, CT.Y);
+        EC_POINT_copy(instance.dlog_instance[i].V, CT.X);
+    }
+
+    BIGNUM *m = BN_new(); 
+
+    BN_random(m);
+    EC_POINT_mul(group, instance.dlog_instance[2].B, NULL, enc_pp.g, m, bn_ctx);
+    EC_POINT_mul(group, instance.dlog_instance[2].A, NULL, instance.dlog_instance[2].B, witness.dlog_witness[2].w, bn_ctx);
+
+
+    //BN_random(witness.w);
+    //BN_set_word(witness.w, 200);
+    //BN_set_word(witness.r, 200);
+
+
+    string chl = "";
+    NIZK_Witness_Encryption_AndR_Prove_Compute_Chl(pp, instance, witness, chl, proof);
+
+    NIZK_Witness_Encryption_AndR_Prove_Compute_Proof(pp, instance, witness, chl, proof);
+
+
+    string res = "";
+    NIZK_Witness_Encryption_AndR_Verify(pp, instance, witness, chl, proof, res);
+
+    if (res == chl){
+	cout << "Range proof accept." << endl;
+    	cout << "chl: " << chl << endl;
+    	cout << "res: " << res << endl;
+    }
+
+    Twisted_ElGamal_PP_free(enc_pp); 
+    Twisted_ElGamal_KP_free(keypair); 
+    Twisted_ElGamal_CT_free(CT); 
+
+    NIZK_Witness_Encryption_AndR_PP_free(pp);
+    NIZK_Witness_Encryption_AndR_Instance_free(instance);
+    NIZK_Witness_Encryption_AndR_Witness_free(witness);
+    NIZK_Witness_Encryption_AndR_Proof_free(proof);
+
+    BN_free(m);
+
+}
+
+int main()
+{  
+    // curve id = NID_secp256k1
+    global_initialize(NID_secp256k1);    
+    // global_initialize(NID_secp256k1); 
+    test_protocol();
+    global_finalize();
+    
+    return 0; 
+}
